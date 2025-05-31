46998df74721ca446e192ec0ecbfca37fddbc762
kailzxh <137093796+kailzxh@users.noreply.github.com>
2025-05-31 18:22:39 +0530

    removed unused declarations

--- src/components/common/SizeSelector.tsx
+++ src/components/common/SizeSelector.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Button } from '../ui/button';
 
 // Data for size options
--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 
 interface SeparatorProps {
   className?: string;
